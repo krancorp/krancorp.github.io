5ee12d6f0c99adfda3c8353e931b8e46fd47ca54
Jakob Schrettenbrunner <jakob.schrettenbrunner@t-online.de>
2016-11-24 14:01:22 +0100

    fix pug base path

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,7 @@ gulp.task('publish', ['build'], () => {
 })
 
 gulp.task('pug', () => {
-	return gulp.src('src/**/*.pug', {base: './'})
+	return gulp.src('./src/**/*.pug', {base: './src'})
 		.pipe(pug())
 		.pipe(gulp.dest('./dist'))
 })
