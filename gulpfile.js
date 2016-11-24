be3b2113211f9f5fcb4525aac54681d66cebb820
Jakob Schrettenbrunner <jakob.schrettenbrunner@t-online.de>
2016-11-24 13:57:17 +0100

    add publish gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,16 @@
 const gulp = require('gulp')
 const pug  = require('gulp-pug')
 const sass = require('gulp-sass')
+const ghPages = require('gulp-gh-pages')
 
-gulp.task('default', ['pug'])
+gulp.task('default', ['build'])
+
+gulp.task('build', ['pug'])
+
+gulp.task('publish', ['build'], () => {
+	return gulp.src('./dist/**/*')
+		.pipe(ghPages({branch: 'master'}))
+})
 
 gulp.task('pug', () => {
 	return gulp.src('src/**/*.pug', {base: './'})
