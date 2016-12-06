558e9c35c334f15e480ee9b94940e8f4b39c9bc0
Jakob Schrettenbrunner <jakob.schrettenbrunner@t-online.de>
2016-12-06 12:47:10 +0100

    Rework gulpfile exctact paths into variables add jquery dependency

--- gulpfile.js
+++ gulpfile.js
@@ -5,29 +5,57 @@ const pug  = require('gulp-pug')
 const sass = require('gulp-sass')
 const ghPages = require('gulp-gh-pages')
 
+const PATH = {
+  npm: './node_modules',
+  assets: './src/assets',
+  dist: './dist',
+  src: './src'
+}
+
+const GLOB = {
+  publish: PATH.dist + '/**/*',
+  pug: './src/**/!(_)*.pug',
+  sass: PATH.assets + '/sass/style.scss',
+  copy: ['./src/CNAME', './src/**/*.?(png|ico)']
+}
+
+const DEST = {
+  sass: PATH.dist + '/assets/css',
+  libs: PATH.dist + '/assets/libs'
+}
+
+const LIBS = [
+  'jquery/dist/jquery.min.js'
+]
+
 gulp.task('default', ['build'])
 
 gulp.task('build', ['pug', 'sass', 'copy'])
 
 gulp.task('publish', ['build'], () => {
-	return gulp.src('./dist/**/*')
+	return gulp.src(GLOB.publish)
 		.pipe(ghPages({branch: 'master', force: true}))
 })
 
 gulp.task('pug', () => {
-	return gulp.src('./src/**/!(_)*.pug', {base: './src'})
+	return gulp.src(GLOB.pug, {base: PATH.src})
 		.pipe(pug())
-		.pipe(gulp.dest('./dist'))
+		.pipe(gulp.dest(PATH.dist))
 })
 
 gulp.task('sass', () => {
-	return gulp.src('./src/assets/sass/style.scss')
+	return gulp.src(GLOB.sass)
 		.pipe(sass())
-		.pipe(gulp.dest('./dist/assets/css'))
+		.pipe(gulp.dest(DEST.sass))
 })
 
-gulp.task('copy', () => {
-  gulp.src('.gitignore').pipe(gulp.dest('./dist'))
-  return gulp.src(['./src/CNAME', './src/**/*.?(png|ico)'], {base: './src'})
-    .pipe(gulp.dest('./dist'))
+gulp.task('copy', ['copyLibs'], () => {
+  gulp.src('.gitignore').pipe(gulp.dest(PATH.dist))
+  return gulp.src(GLOB.copy, {base: PATH.src})
+    .pipe(gulp.dest(PATH.dist))
+})
+
+gulp.task('copyLibs', () => {
+  return gulp.src(LIBS.map(l => PATH.npm + '/' + l))
+    .pipe(gulp.dest(DEST.libs))
 })
